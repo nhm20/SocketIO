3d9050550d67901f205460e0284975e5c1c4c358
nhm20 <nmo212182@gmail.com>
2024-07-27 09:42:09 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const io = require('socket.io')(3000, {
+     cors: {
+          origin: ['http://localhost:8081']
+     }
+});  
+
+io.on('connection', (socket) => {
+     console.log('New user connected', socket.id);
+     socket.on('custom-event', (num, str, obj) => {
+          console.log(num, str, obj);
+     });
+});
+
